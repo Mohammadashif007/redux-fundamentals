fb2d87f9d01646898bee8c6ef8350139ef7a7d09
Mohammad Ashif <mohammadashif510@gmail.com>
2025-07-01 06:17:26 +0600

    dark mode added done

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,6 +3,8 @@
 //         <nav className="bg-gray-900 h-10">
 
 import { Link } from "react-router";
+import { ThemeProvider } from "../../providers/theme-provider";
+import { ModeToggle } from "../mode-toggle/mode-toggle";
 
 //         </nav>
 //     );
@@ -12,19 +14,24 @@ import { Link } from "react-router";
 
 const Navbar = () => {
     return (
-        <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+        <nav className="bg-orange-600 shadow-md px-6 py-4 flex justify-between items-center">
             {/* Logo */}
-            <div className="text-2xl font-bold text-blue-600">MyLogo</div>
+            <div className="text-2xl font-bold text-white">MyLogo</div>
 
             {/* Navigation Links */}
             <div className="flex gap-5">
-                <Link  to="/" className="hover:text-blue-500 cursor-pointer">
+                <Link to="/" className="hover:text-blue-500 cursor-pointer">
                     Task
                 </Link>
                 <Link to="/user" className="hover:text-blue-500 cursor-pointer">
                     User
                 </Link>
+                <div>
+                    <ModeToggle></ModeToggle>
+                </div>
+                
             </div>
+                    
         </nav>
     );
 };
